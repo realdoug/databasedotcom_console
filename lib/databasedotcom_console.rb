6a6e13a98cb036cedfec1bd1d03cac96e58935f5
Doug Friedman <dfriedman@mavensconsulting.com>
2013-01-29 20:07:46 -0500

    include new_client method for spinnig up dbdc connections

--- lib/databasedotcom_console.rb
+++ lib/databasedotcom_console.rb
@@ -2,9 +2,23 @@ require "databasedotcom_console/version"
 require "databasedotcom"
 
 module DatabasedotcomConsole
-  # class Chatter
-  #   def say_hello
-  #     puts 'This is zerp. Over'
-  #   end
-  # end
+	clients = []
+
+	## Return a list of all clients
+	def self.clients
+		clients
+	end
+
+	## Create a new client for use with multiple orgs
+	def self.new_client(host, username, password)
+		client = Databasedotcom::Client.new(
+	          	:client_id => '3MVG9QDx8IX8nP5RUXIFbi3L4rSVruiBU4O_ozvkJiU0aGZDBxfsF6XDhngJf6Ha2fDNniyDxpt0Gb9Pp.2Tk',
+	          	:client_secret => '7712607496533706277',
+		  				:host => host
+	        	)
+		client.authenticate(:username => username, :password => password)
+		#clients << client
+		client
+	end
+
 end
